5fb2d6bbb7a16fafe9664f4fe13203189f7e6321
GuiCT <gtomiasi@gmail.com>
2022-09-02 14:20:07 -0300

    atualizando Professor.java
    formatando código

--- src/Professor.java
+++ src/Professor.java
@@ -2,12 +2,10 @@ import java.time.LocalDate;
 import java.util.Date;
 
 public class Professor extends Usuario {
-
 	private LocalDate data_contratacao;
 
 	public Professor(LocalDate data_contratacao) {
 		super();
 		this.data_contratacao = data_contratacao;
 	}
-
 }
