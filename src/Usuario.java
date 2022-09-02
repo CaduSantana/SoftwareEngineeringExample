24691e00b97ec52337dc9aba139887c81c78d830
GuiCT <gtomiasi@gmail.com>
2022-09-02 14:22:32 -0300

    atualizando Usuario.java
    formatando código
    incluindo comentários para melhor entendimento

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,17 +1,12 @@
 import java.util.ArrayList;
 
 public class Usuario {
-
 	private String nome;
-
 	private String endereco;
 
-	public boolean login(String email, String senha) {
-		return false;
-	}
-
 	public Usuario() {
-		// TODO Auto-generated constructor stub
+		// Construtor vazio para facilitar o uso de métodos que precisam de uma
+		// instância de Usuário
 	}
 
 	public Usuario(String nome, String endereco) {
@@ -19,6 +14,11 @@ public class Usuario {
 		this.endereco = endereco;
 	}
 
-	
-	
+	public boolean login(String email, String senha) {
+		return false;
+		// A lógica de autenticação não é implementada por ora, sendo necessário
+		// maior conhecimento na área de Segurança da Informação, criptografia
+		// e um banco de dados para armazenar as informações de autenticação de cada
+		// usuário que realize o cadastro no sistema.
+	}
 }
